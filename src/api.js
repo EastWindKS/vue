50f3b6543484e91269e3dcf64aeec6271eda7d62
Yehor Yurchenko <immemorial.ks@gmail.com>
2022-02-21 17:01:59 +0200

    added new functionality

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
 const apiKey = "5581e5622195b446fa57c011b5a6b50aac60bb89f06a336e0eb268e66022a805";
-const URL = "https://min-api.cryptocompare.com/data/price?";
+const URL = "https://min-api.cryptocompare.com/data/";
 
 export const getDataByName = async name => {
-    return (await fetch(URL + `fsym=${name}&tsyms=USD&api_key=${apiKey}`)).json();
+    return (await fetch(URL + `price?fsym=${name}&tsyms=USD&api_key=${apiKey}`)).json();
+}
+
+export const getCoinsList = async ()=> {
+    return (await fetch(URL + "all/coinlist?summary=true")).json();
 }
